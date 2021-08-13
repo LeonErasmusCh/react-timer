6c8fc84fe84d9076edc5b249a84c33548b5abafd
LeonErasmusCh <leon@santiagobiltong.cl>
2021-08-13 21:14:42 +0000

    commit 1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,21 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import { ProgressPlugin } from "webpack";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container-fluid">
+				<div className="row justify-content-center">
+					<h1 className="display-2">
+						<span className="badge bg-dark text-light m-1">
+							{props.digit1}
+						</span>
+					</h1>
+				</div>
+			</div>
+		</>
 	);
 }
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,82 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
 import "bootstrap";
-
-//include your index.scss file into the bundle
+import PropTypes from "prop-types";
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+export function SimpleCounter(props) {
+	return (
+		<>
+			<div className="container-fluid bg-dark">
+				<div className="row justify-content-center p-3">
+					<div className="display-2">
+						<span className="badge bg-dark text-light m-1">
+							<i className="far fa-clock"></i>
+						</span>
+					</div>
+					<div className="display-2">
+						<span className="badge bg-dark text-light m-1">
+							{props.digitFour % 10}
+						</span>
+					</div>
+					<div className="display-2">
+						<span className="badge bg-dark text-light m-1">
+							{props.digitThree % 10}
+						</span>
+					</div>
+					<div className="display-2">
+						<span className="badge bg-dark text-light m-1">
+							{props.digitTwo % 10}
+						</span>
+					</div>
+					<div className="display-2">
+						<span className="badge bg-dark text-light m-1">
+							{props.digitOne % 10}
+						</span>
+					</div>
+				</div>
+				<div className="container-fluid">
+					<div className="row justify-content-center">
+						<button
+							onClick={myStopFunction}
+							type="button"
+							class="btn btn-danger m-2">
+							Stop Timer
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+const interval = setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+	console.log(four, three, two, one);
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
+
+function myStopFunction() {
+	clearInterval(interval);
+}
+
+SimpleCounter.PropTypes = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number
+};
